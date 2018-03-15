9b475670ea680fb66f9ff3e54a9863076a9c1d65
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-03-14 23:55:57 -0400

    uploading news aggregator exercise done previously with Sinatra

---
+++ server.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+	require 'csv'
+	require 'pry'
+
+
+	set :bind, '0.0.0.0'  # bind to all interfaces
+
+	get '/' do
+	  redirect '/articles'
+	end
+
+	get '/articles/new' do
+	  # @articles = File.readlines('news.txt')
+	  erb :articles
+	end
+
+	get '/articles' do
+
+	  @csvarticles = CSV.readlines("articles.csv")
+	  erb :index
+	end
+
+	post '/articles/new' do
+	  article_title = params['article_title']
+	  article_url = params['article_url']
+	  article_description = params['article_description']
+	  individual_article = [article_title, article_url, article_description]
+
+	  # @objectarray = []
+	  # CSV.foreach('articles.csv') do |row|
+	  #   newsobject = Object.new(row[0], row[1], row[2])
+	  #   @objectarray << newsobject
+
+	  CSV.open("articles.csv", "a") do |csv|
+	    csv << individual_article
+
+	  end
+	  redirect '/articles/new'
+	end
